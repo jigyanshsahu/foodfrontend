d300db3af912220966b0b236f232d2896970d722
Jigynash sahu <jigyanshsahu8@gmail.com>
2025-12-16 21:40:02 +0530

    Fix StoreContext import casing everywhere

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import storecontextProvider from './context/storecontext.jsx'
+import { StoreContextProvider } from "./context/storecontext.jsx";
+
 
 createRoot(document.getElementById('root')).render(
  <BrowserRouter>
